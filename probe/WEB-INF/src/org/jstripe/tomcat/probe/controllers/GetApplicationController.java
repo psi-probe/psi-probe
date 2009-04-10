53675069561856662825f52f0323b01248964ddd
Mark Lewis <malfunction84@gmail.com>
2009-04-10 21:26:09 +0000

    (LambdaProbe trunk:324 by andy) modified application summary to refresh runtime info periodically (not final)

--- probe/WEB-INF/src/org/jstripe/tomcat/probe/controllers/GetApplicationController.java
+++ probe/WEB-INF/src/org/jstripe/tomcat/probe/controllers/GetApplicationController.java
@@ -8,23 +8,35 @@
  *  IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  *  WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
-
 package org.jstripe.tomcat.probe.controllers;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.apache.catalina.Context;
 import org.jstripe.tomcat.probe.tools.ApplicationUtils;
-import org.springframework.web.bind.ServletRequestUtils;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Retrieves a web application summary information
+ * Retrieves Application model object populated with application information
  * <p/>
  * Author: Andy Shapoval
  */
-public class AppSummaryController extends ContextHandlerController {
+public class GetApplicationController extends ContextHandlerController {
+    /**
+     * denotes whether extended application information and statistics should be collected
+     */
+    private boolean extendedInfo = false;
+
+    public boolean isExtendedInfo() {
+        return extendedInfo;
+    }
+
+    public void setExtendedInfo(boolean extendedInfo) {
+        this.extendedInfo = extendedInfo;
+    }
+
     protected ModelAndView handleContext(String contextName, Context context,
                                          HttpServletRequest request, HttpServletResponse response) throws Exception {
 
@@ -32,9 +44,9 @@ public class AppSummaryController extends ContextHandlerController {
 
         boolean calcSize = ServletRequestUtils.getBooleanParameter(request, "size", false) && request.isUserInRole(privelegedRole);
 
-        ModelAndView mv = new ModelAndView(getViewName(), "app",
-                ApplicationUtils.getApplication(context, getContainerWrapper().getResourceResolver(), calcSize));
-        
+        ModelAndView mv = new ModelAndView(getViewName(), "app", ApplicationUtils.getApplication(
+                context, isExtendedInfo() ? getContainerWrapper().getResourceResolver() : null, calcSize));
+
         if (! getContainerWrapper().getResourceResolver().supportsPrivateResources()) {
             mv.addObject("no_resources", Boolean.TRUE);
         }
