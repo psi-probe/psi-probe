a281ad3b184a0202958e5aa4320c7e36ff0caad8
MALfunction84@gmail.com <MALfunction84@gmail.com@c64a9a66-49c4-11de-acc5-7fb027fc3da9>
2009-12-13 21:03:14 +0000

    Update issue 12 Updating links to GPLv2 license.

--- trunk/core/src/main/java/org/jstripe/tomcat/probe/beans/stats/providers/MultipleSeriesProvider.java
+++ trunk/core/src/main/java/org/jstripe/tomcat/probe/beans/stats/providers/MultipleSeriesProvider.java
@@ -1,14 +1,13 @@
 /*
- * Licensed under the GPL License. You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the GPL License.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
  *
- *   http://probe.jstripe.com/d/license.shtml
+ *     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
- * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
-
 package org.jstripe.tomcat.probe.beans.stats.providers;
 
 import org.jfree.data.xy.DefaultTableXYDataset;
