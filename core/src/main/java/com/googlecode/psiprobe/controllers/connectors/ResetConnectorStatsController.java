904fc3a31b482b90d57eb0e2c9d580dc9d83e52f
Mark Lewis <malfunction84@gmail.com>
2010-05-04 22:16:10 +0000

    Update issue 46 - Adding controller.  No UI yet.

---
+++ core/src/main/java/com/googlecode/psiprobe/controllers/connectors/ResetConnectorStatsController.java
@@ -0,0 +1,33 @@
+package com.googlecode.psiprobe.controllers.connectors;
+
+import com.googlecode.psiprobe.beans.stats.collectors.ConnectorStatsCollectorBean;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
+import org.springframework.web.servlet.view.RedirectView;
+
+/**
+ *
+ * @author <a href="mailto:mlewis@itos.uga.edu">Mark Lewis</a>
+ */
+public class ResetConnectorStatsController extends ParameterizableViewController {
+
+    ConnectorStatsCollectorBean collectorBean;
+
+    public ConnectorStatsCollectorBean getCollectorBean() {
+        return collectorBean;
+    }
+
+    public void setCollectorBean(ConnectorStatsCollectorBean collectorBean) {
+        this.collectorBean = collectorBean;
+    }
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String connectorName = ServletRequestUtils.getRequiredStringParameter(request, "cn");
+        collectorBean.reset(connectorName);
+        return new ModelAndView(new RedirectView(request.getContextPath() + getViewName()));
+    }
+
+}
