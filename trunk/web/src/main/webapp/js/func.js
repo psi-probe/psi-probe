a604fa7d6be82d73f5c047515f8f2bb9fd48dfc9
MALfunction84@gmail.com <MALfunction84@gmail.com@c64a9a66-49c4-11de-acc5-7fb027fc3da9>
2010-05-22 21:58:16 +0000

    - Converting spaces to tabs. - Reformatting.

--- trunk/web/src/main/webapp/js/func.js
+++ trunk/web/src/main/webapp/js/func.js
@@ -10,21 +10,21 @@
  */
 
 function inverse($f) {
-    for ($i = 0; $i < $f.elements.length; $i++) {
-        if ($f.elements[$i].type == "checkbox") {
-            $f.elements[$i].checked = !$f.elements[$i].checked;
-        }
-    }
-    return false;
+	for ($i = 0; $i < $f.elements.length; $i++) {
+		if ($f.elements[$i].type == "checkbox") {
+			$f.elements[$i].checked = !$f.elements[$i].checked;
+		}
+	}
+	return false;
 }
 
 function checkAll($f) {
-    for ($i = 0; $i < $f.elements.length; $i++) {
-        if ($f.elements[$i].type == "checkbox") {
-            $f.elements[$i].checked = true;
-        }
-    }
-    return false;
+	for ($i = 0; $i < $f.elements.length; $i++) {
+		if ($f.elements[$i].type == "checkbox") {
+			$f.elements[$i].checked = true;
+		}
+	}
+	return false;
 }
 
 /**
@@ -32,151 +32,164 @@ function checkAll($f) {
  */
 Ajax.ImgUpdater = Class.create();
 Ajax.ImgUpdater.prototype = {
-    initialize: function(imgID, timeout, newSrc) {
-        this.img = document.getElementById(imgID);
-        if (newSrc) {
-            this.src = newSrc;
-        } else {
-            this.src = this.img.src;
-        }
-        this.timeout = timeout;
-        this.start();
-    },
+	initialize: function(imgID, timeout, newSrc) {
+		this.img = document.getElementById(imgID);
+		if (newSrc) {
+			this.src = newSrc;
+		} else {
+			this.src = this.img.src;
+		}
+		this.timeout = timeout;
+		this.start();
+	},
 
-    start: function() {
-        var now = new Date();
-        this.img.src = this.src + '&t=' + now.getTime();
-        this.timer = setTimeout(this.start.bind(this), this.timeout * 1000);
-    },
+	start: function() {
+		var now = new Date();
+		this.img.src = this.src + '&t=' + now.getTime();
+		this.timer = setTimeout(this.start.bind(this), this.timeout * 1000);
+	},
 
-    stop: function() {
-        if (this.timer) clearTimeout(this.timer);
-    }
+	stop: function() {
+		if (this.timer) clearTimeout(this.timer);
+	}
 }
 
 function togglePanel(container, remember_url) {
-    if (Element.getStyle(container, "display") == 'none') {
-        if (remember_url) {
-            new Ajax.Request(remember_url, {method:'get',asynchronous:true, parameters: 'state=on'});
-        }
-        if (document.getElementById('invisible_' + container)) {
-            Element.hide('invisible_' + container);
-        }
-        if (document.getElementById('visible_' + container)) {
-            Element.show('visible_' + container);
-        }
+	if (Element.getStyle(container, "display") == 'none') {
+		if (remember_url) {
+			new Ajax.Request(remember_url, {
+				method:'get',
+				asynchronous:true,
+				parameters: 'state=on'
+			});
+		}
+		if (document.getElementById('invisible_' + container)) {
+			Element.hide('invisible_' + container);
+		}
+		if (document.getElementById('visible_' + container)) {
+			Element.show('visible_' + container);
+		}
 
-        Effect.Grow(container);
-    } else {
-        if (remember_url) {
-            new Ajax.Request(remember_url, {method:'get',asynchronous:true, parameters: 'state=off'});
-        }
-        if (document.getElementById('visible_' + container)) {
-            Element.hide('visible_' + container);
-        }
-        if (document.getElementById('invisible_' + container)) {
-            Element.show('invisible_' + container);
-        }
-        Effect.Shrink(container);
-    }
-    return false;
+		Effect.Grow(container);
+	} else {
+		if (remember_url) {
+			new Ajax.Request(remember_url, {
+				method:'get',
+				asynchronous:true,
+				parameters: 'state=off'
+			});
+		}
+		if (document.getElementById('visible_' + container)) {
+			Element.hide('visible_' + container);
+		}
+		if (document.getElementById('invisible_' + container)) {
+			Element.show('invisible_' + container);
+		}
+		Effect.Shrink(container);
+	}
+	return false;
 }
 
 function scaleImage(v, min, max) {
-    var images = document.getElementsByClassName('scale-image');
-    w = (max - min) * v + min;
-    for (i = 0; i < images.length; i++) {
-        images[i].style.width = w + 'px';
-    }
+	var images = document.getElementsByClassName('scale-image');
+	w = (max - min) * v + min;
+	for (i = 0; i < images.length; i++) {
+		images[i].style.width = w + 'px';
+	}
 }
 
 function toggleAndReloadPanel(container, url) {
-    if (Element.getStyle(container, "display") == 'none') {
-        new Ajax.Updater(container, url);
-        Effect.BlindDown(container);
-    } else {
-        Effect.Shrink(container);
-    }
+	if (Element.getStyle(container, "display") == 'none') {
+		new Ajax.Updater(container, url);
+		Effect.BlindDown(container);
+	} else {
+		Effect.Shrink(container);
+	}
 }
 
 function getWindowHeight() {
-    var myHeight = 0;
-    if (typeof( window.innerHeight ) == 'number') {
-        //Non-IE
-        myHeight = window.innerHeight;
-    } else if (document.documentElement && document.documentElement.clientHeight) {
-        //IE 6+ in 'standards compliant mode'
-        myHeight = document.documentElement.clientHeight;
-    } else if (document.body && document.body.clientHeight) {
-        //IE 4 compatible
-        myHeight = document.body.clientHeight;
-    }
-    return myHeight;
+	var myHeight = 0;
+	if (typeof( window.innerHeight ) == 'number') {
+		//Non-IE
+		myHeight = window.innerHeight;
+	} else if (document.documentElement && document.documentElement.clientHeight) {
+		//IE 6+ in 'standards compliant mode'
+		myHeight = document.documentElement.clientHeight;
+	} else if (document.body && document.body.clientHeight) {
+		//IE 4 compatible
+		myHeight = document.body.clientHeight;
+	}
+	return myHeight;
 }
 
 function getWindowWidth() {
-    var myWidth = 0;
-    if (typeof( window.innerWidth ) == 'number') {
-        //Non-IE
-        myWidth = window.innerWidth;
-    } else if (document.documentElement && document.documentElement.clientWidth) {
-        //IE 6+ in 'standards compliant mode'
-        myWidth = document.documentElement.clientWidth;
-    } else if (document.body && document.body.clientWidth) {
-        //IE 4 compatible
-        myWidth = document.body.clientWidth;
-    }
-    return myWidth;
+	var myWidth = 0;
+	if (typeof( window.innerWidth ) == 'number') {
+		//Non-IE
+		myWidth = window.innerWidth;
+	} else if (document.documentElement && document.documentElement.clientWidth) {
+		//IE 6+ in 'standards compliant mode'
+		myWidth = document.documentElement.clientWidth;
+	} else if (document.body && document.body.clientWidth) {
+		//IE 4 compatible
+		myWidth = document.body.clientWidth;
+	}
+	return myWidth;
 }
 
 var helpTimerID;
 
 function setupHelpToggle(url) {
-    rules = {
-        'li#abbreviations': function(element) {
-            element.onclick = function() {
-                help_container = 'help';
-                if (Element.getStyle(help_container, "display") == 'none') {
-                    new Ajax.Updater(help_container, url);
-                }
-                Effect.toggle(help_container, 'appear');
-                if (helpTimerID) clearTimeout(helpTimerID)
-                helpTimerID = setTimeout('Effect.Fade("' + help_container + '")', 15000);
-                return false;
-            }
-        }
-    }
-    Behaviour.register(rules);
+	rules = {
+		'li#abbreviations': function(element) {
+			element.onclick = function() {
+				help_container = 'help';
+				if (Element.getStyle(help_container, "display") == 'none') {
+					new Ajax.Updater(help_container, url);
+				}
+				Effect.toggle(help_container, 'appear');
+				if (helpTimerID) clearTimeout(helpTimerID)
+				helpTimerID = setTimeout('Effect.Fade("' + help_container + '")', 15000);
+				return false;
+			}
+		}
+	}
+	Behaviour.register(rules);
 }
 
 function addAjaxTooltip(activator, tooltip, url) {
-    Tooltip.closeText = null;
-    Tooltip.autoHideTimeout = null;
-    Tooltip.showMethod = function(e) {
-        Effect.Appear(e, {to: 0.9});
-    }
+	Tooltip.closeText = null;
+	Tooltip.autoHideTimeout = null;
+	Tooltip.showMethod = function(e) {
+		Effect.Appear(e, {
+			to: 0.9
+		});
+	}
 
-    Tooltip.add(activator, tooltip);
-    tt_container = $$('#' + tooltip + ' .tt_content')[0];
-    Event.observe(activator, 'click', function(e) {
+	Tooltip.add(activator, tooltip);
+	tt_container = $$('#' + tooltip + ' .tt_content')[0];
+	Event.observe(activator, 'click', function(e) {
 
-        t_title = $('tt_title');
+		t_title = $('tt_title');
 
-        if (t_title) t_title.hide();
+		if (t_title) t_title.hide();
 
-        tt_container.style.width = '300px';
+		tt_container.style.width = '300px';
 
-        tt_container.innerHTML = '<div class="ajax_activity">&nbsp;</div>';
-        new Ajax.Updater(tt_container, url, {method: 'get'}).onComplete = function () {
-            tt_container.style.width = null;
-            the_title = $('tooltip_title');
-            t_title = $('tt_title');
+		tt_container.innerHTML = '<div class="ajax_activity">&nbsp;</div>';
+		new Ajax.Updater(tt_container, url, {
+			method: 'get',
+			onComplete: function() {
+				tt_container.style.width = null;
+				the_title = $('tooltip_title');
+				t_title = $('tt_title');
 
-            if (the_title && t_title) {
-                the_title.hide();
-                t_title.innerHTML = the_title.innerHTML;
-                t_title.show();
-            }
-        };
-    });
+				if (the_title && t_title) {
+					the_title.hide();
+					t_title.innerHTML = the_title.innerHTML;
+					t_title.show();
+				}
+			}
+		});
+	});
 }
