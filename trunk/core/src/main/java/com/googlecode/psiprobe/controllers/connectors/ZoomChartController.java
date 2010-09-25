787bb40ba78987f45bbd1bf60348a443c68293f9
MALfunction84@gmail.com <MALfunction84@gmail.com@c64a9a66-49c4-11de-acc5-7fb027fc3da9>
2010-09-25 01:22:46 +0000

    Update issue 111 - Addressing refresh interval and message on zoomed-in Connector Stats graph.

---
+++ trunk/core/src/main/java/com/googlecode/psiprobe/controllers/connectors/ZoomChartController.java
@@ -0,0 +1,29 @@
+package com.googlecode.psiprobe.controllers.connectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
+
+/**
+ *
+ * @author <a href="mailto:mlewis@itos.uga.edu">Mark Lewis</a>
+ */
+public class ZoomChartController extends ParameterizableViewController {
+
+    private long collectionPeriod;
+
+    public long getCollectionPeriod() {
+        return collectionPeriod;
+    }
+
+    public void setCollectionPeriod(long collectionPeriod) {
+        this.collectionPeriod = collectionPeriod;
+    }
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return super.handleRequestInternal(request, response)
+                .addObject("collectionPeriod", Long.valueOf(getCollectionPeriod()));
+    }
+
+}
