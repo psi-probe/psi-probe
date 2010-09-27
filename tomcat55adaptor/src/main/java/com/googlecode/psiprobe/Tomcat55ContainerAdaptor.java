477042ca64ecc4e9cae8066bc2f5a15319e8aed2
Mark Lewis <malfunction84@gmail.com>
2010-09-27 21:45:35 +0000

    Update issue 91 Status: Fixed - Creating Tomcat 6.0 Adaptor.

--- tomcat55adaptor/src/main/java/com/googlecode/psiprobe/Tomcat55ContainerAdaptor.java
+++ tomcat55adaptor/src/main/java/com/googlecode/psiprobe/Tomcat55ContainerAdaptor.java
@@ -50,12 +50,7 @@ public class Tomcat55ContainerAdaptor extends AbstractTomcatContainer {
     }
 
     public boolean canBoundTo(String binding) {
-        return binding != null && (
-                binding.startsWith("Apache Tomcat/5.5")
-                || binding.startsWith("Apache Tomcat/6.0")
-                || binding.startsWith("Apache Tomcat/7.0")
-                || binding.startsWith("JBossWeb/2.0")
-                || binding.startsWith("JBossWeb/2.1"));
+        return binding != null && (binding.startsWith("Apache Tomcat/5.5"));
     }
 
     public Context findContext(String name) {
