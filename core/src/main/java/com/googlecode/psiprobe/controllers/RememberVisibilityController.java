fccd384bd2b52709b728559dfda766c209a7ef58
Mark Lewis <malfunction84@gmail.com>
2010-12-08 23:33:15 +0000

    Moving "remember" AJAX controller up one level since it is not limited to connectors as of r212.

--- core/src/main/java/com/googlecode/psiprobe/controllers/RememberVisibilityController.java
+++ core/src/main/java/com/googlecode/psiprobe/controllers/RememberVisibilityController.java
@@ -8,7 +8,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
-package com.googlecode.psiprobe.controllers.connectors;
+package com.googlecode.psiprobe.controllers;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
