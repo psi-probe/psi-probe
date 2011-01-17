98ad211dd2e38e5e09811bd115a873093a75c76d
MALfunction84@gmail.com <MALfunction84@gmail.com@c64a9a66-49c4-11de-acc5-7fb027fc3da9>
2011-01-17 01:35:11 +0000

    Adding license to a file that was created as part of issue 111.

--- trunk/core/src/main/java/com/googlecode/psiprobe/controllers/apps/AllAppStatsController.java
+++ trunk/core/src/main/java/com/googlecode/psiprobe/controllers/apps/AllAppStatsController.java
@@ -1,8 +1,13 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Licensed under the GPL License.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
-
 package com.googlecode.psiprobe.controllers.apps;
 
 import javax.servlet.http.HttpServletRequest;
