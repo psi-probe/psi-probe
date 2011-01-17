32fc9cfd8237293c4af4c3258afa7e7f6e63ec78
Mark Lewis <malfunction84@gmail.com>
2011-01-17 01:32:42 +0000

    Update issue 9 Abstracting log resolution into its own class.

--- core/src/main/java/com/googlecode/psiprobe/controllers/logs/ListLogsController.java
+++ core/src/main/java/com/googlecode/psiprobe/controllers/logs/ListLogsController.java
@@ -10,34 +10,18 @@
  */
 package com.googlecode.psiprobe.controllers.logs;
 
-import com.googlecode.psiprobe.controllers.TomcatContainerController;
-import com.googlecode.psiprobe.model.Application;
-import com.googlecode.psiprobe.model.DisconnectedLogDestination;
-import com.googlecode.psiprobe.tools.ApplicationUtils;
-import com.googlecode.psiprobe.tools.Instruments;
-import com.googlecode.psiprobe.tools.logging.FileLogAccessor;
-import com.googlecode.psiprobe.tools.logging.LogDestination;
-import com.googlecode.psiprobe.tools.logging.catalina.CatalinaLoggerAccessor;
-import com.googlecode.psiprobe.tools.logging.commons.CommonsLoggerAccessor;
-import com.googlecode.psiprobe.tools.logging.jdk.Jdk14ManagerAccessor;
-import com.googlecode.psiprobe.tools.logging.log4j.Log4JManagerAccessor;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.LinkedList;
+import com.googlecode.psiprobe.beans.LogResolverBean;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.catalina.Context;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
 
-public class ListLogsController extends TomcatContainerController {
+public class ListLogsController extends ParameterizableViewController {
 
     private String errorView;
-    private List stdoutFiles = new ArrayList();
+    private LogResolverBean logResolver;
 
     public String getErrorView() {
         return errorView;
@@ -47,168 +31,23 @@ public class ListLogsController extends TomcatContainerController {
         this.errorView = errorView;
     }
 
-    public List getStdoutFiles() {
-        return stdoutFiles;
+    public LogResolverBean getLogResolver() {
+        return logResolver;
     }
 
-    public void setStdoutFiles(List stdoutFiles) {
-        this.stdoutFiles = stdoutFiles;
+    public void setLogResolver(LogResolverBean logResolver) {
+        this.logResolver = logResolver;
     }
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        if (Instruments.isInitialized()) {
-            List allAppenders = new ArrayList();
-
-            //
-            // interrogate classloader hierarchy
-            //
-            ClassLoader cl2 = Thread.currentThread().getContextClassLoader().getParent();
-            while (cl2 != null) {
-                interrogateClassLoader(cl2, null, allAppenders);
-                cl2 = cl2.getParent();
-            }
-
-            //
-            // check for known stdout files, such as "catalina.out"
-            //
-            interrogateStdOutFiles(allAppenders);
-
-            //
-            // interrogate webapp classloaders and avilable loggers
-            //
-            List contexts = getContainerWrapper().getTomcatContainer().findContexts();
-            interrogateApplicationClassLoaders(contexts, allAppenders);
-
-            //
-            // this list has to guarantee the order in which elements are added
-            //
-            List uniqueList = new LinkedList();
-
-            boolean  showAll = ServletRequestUtils.getBooleanParameter(request, "apps", false);
-            Comparator cmp = new LogDestinationComparator(showAll);
-
-            Collections.sort(allAppenders, cmp);
-            for (int i = 0; i < allAppenders.size(); i++) {
-                LogDestination dest = (LogDestination) allAppenders.get(i);
-                if (Collections.binarySearch(uniqueList, dest, cmp) < 0) {
-                    if (showAll || dest.getFile() == null || dest.getFile().exists()) {
-                        uniqueList.add(new DisconnectedLogDestination(dest));
-                    }
-                }
-            }
-
+        boolean showAll = ServletRequestUtils.getBooleanParameter(request, "apps", false);
+        List uniqueList = logResolver.getLogDestinations(showAll);
+        if (uniqueList != null) {
             request.getSession(true).setAttribute("logs", uniqueList);
             return new ModelAndView(getViewName());
         } else {
             return new ModelAndView(errorView);
-        }
-    }
-
-    private void interrogateApplicationClassLoaders(List contexts, List allAppenders) {
-        for (int i = 0; i < contexts.size(); i++) {
-
-            Context ctx = (Context) contexts.get(i);
-            Application application = ApplicationUtils.getApplication(ctx);
-
-            ClassLoader cl = ctx.getLoader().getClassLoader();
-
-            try {
-                Object contextLogger = getContainerWrapper().getTomcatContainer().getLogger(ctx);
-                if (contextLogger != null) {
-                    if (contextLogger.getClass().getName().startsWith("org.apache.commons.logging")) {
-                        CommonsLoggerAccessor commonsAccessor = new CommonsLoggerAccessor();
-                        commonsAccessor.setTarget(contextLogger);
-                        commonsAccessor.setApplication(application);
-                        allAppenders.addAll(commonsAccessor.getDestinations());
-                    } else if (contextLogger.getClass().getName().startsWith("org.apache.catalina.logger")) {
-                        CatalinaLoggerAccessor catalinaAccessor = new CatalinaLoggerAccessor();
-                        catalinaAccessor.setApplication(application);
-                        catalinaAccessor.setTarget(contextLogger);
-                        allAppenders.add(catalinaAccessor);
-                    }
-                }
-            } catch (Throwable e) {
-                logger.error("Could not interrogate context logger for " + ctx.getName() + ". Enable debug logging to see the trace stack");
-                logger.debug(e);
-                //
-                // make sure we always re-throw ThreadDeath
-                //
-                if (e instanceof ThreadDeath) {
-                    throw (ThreadDeath) e;
-                }
-            }
-
-            if (application.isAvailable()) {
-                ClassLoader thisCl = Thread.currentThread().getContextClassLoader();
-                Thread.currentThread().setContextClassLoader(cl);
-                try {
-                    interrogateClassLoader(cl, application, allAppenders);
-                } catch (Exception e) {
-                    logger.error("Could not interrogate classloader loggers for " + ctx.getName() + ". Enable debug logging to see the trace stack");
-                    logger.debug(e);
-                } finally {
-                    Thread.currentThread().setContextClassLoader(thisCl);
-                }
-            }
-        }
-    }
-
-    private void interrogateClassLoader(ClassLoader cl, Application application, List appenders) throws Exception {
-
-        Jdk14ManagerAccessor jdk14accessor = Jdk14ManagerAccessor.create(cl);
-        if (jdk14accessor != null) {
-            jdk14accessor.setApplication(application);
-            appenders.addAll(jdk14accessor.getHandlers());
-        }
-
-        // check for Log4J loggers
-        Log4JManagerAccessor log4JAccessor = Log4JManagerAccessor.create(cl);
-        if (log4JAccessor != null) {
-            log4JAccessor.setApplication(application);
-            appenders.addAll(log4JAccessor.getAppenders());
-        }
-    }
-
-    private void interrogateStdOutFiles(List appenders) {
-        for (Iterator it = stdoutFiles.iterator(); it.hasNext(); ) {
-            String fileName = (String) it.next();
-            File stdout = new File(System.getProperty("catalina.base"), "logs/" + fileName);
-            if (stdout.exists()) {
-                FileLogAccessor fla = new FileLogAccessor();
-                fla.setFile(stdout);
-                appenders.add(fla);
-            }
-        }
-
-    }
-
-    private static class LogDestinationComparator implements Comparator {
-
-        private boolean showAll;
-
-        public LogDestinationComparator(boolean showAll) {
-            this.showAll = showAll;
-        }
-
-        public int compare(Object o1, Object o2) {
-            LogDestination d1 = (LogDestination) o1;
-            LogDestination d2 = (LogDestination) o2;
-            String name1, name2;
-            if (showAll) {
-                String appName1 = d1.getApplication() != null ? d1.getApplication().getName() : "";
-                String appName2 = d2.getApplication() != null ? d2.getApplication().getName() : "";
-                String logClass1 = d1.getLogClass();
-                String logClass2 = d1.getLogClass();
-                name1 = appName1 + logClass1 + (d1.getFile() == null ? "" : d1.getFile().getAbsolutePath());
-                name2 = appName2 + logClass2 + (d2.getFile() == null ? "" : d2.getFile().getAbsolutePath());
-            } else {
-                File f1 = d1.getFile();
-                File f2 = d2.getFile();
-                name1 = (f1 == null ? "" : f1.getAbsolutePath());
-                name2 = (f2 == null ? "" : f2.getAbsolutePath());
-            }
-            return name1.compareTo(name2);
         }
     }
 
