28352b8b66c7dfec27515d1a17b991a3a5ab2b44
Mark Lewis <malfunction84@gmail.com>
2011-02-07 19:29:32 +0000

    Update issue 88 - Replacing images with red/blue ones using a new naming convention. - Adding optional value2 attribute to <probe:score> tag. - Implementing display of second value as blue.

--- core/src/main/java/com/googlecode/psiprobe/tags/VisualScoreTag.java
+++ core/src/main/java/com/googlecode/psiprobe/tags/VisualScoreTag.java
@@ -20,6 +20,7 @@ import javax.servlet.jsp.tagext.BodyTagSupport;
 public class VisualScoreTag extends BodyTagSupport {
 
     private double value;
+    private double value2 = 0;
     private double minValue = 0;
     private double maxValue = 100;
     private int partialBlocks = 1;
@@ -32,43 +33,73 @@ public class VisualScoreTag extends BodyTagSupport {
 
         try {
             double unitSize = (maxValue - minValue) / (fullBlocks * partialBlocks);
+            double blockWidth = unitSize * partialBlocks;
 
-            int fullBlockCount = (int) Math.floor(value / (unitSize * partialBlocks));
-            int partialBlockIndex = (int) Math.floor((value - fullBlockCount * unitSize * partialBlocks) / unitSize);
+            int fullRedBlockCount = (int) Math.floor(value / blockWidth);
+            int partialRedBlockIndex = (int) Math.floor((value - fullRedBlockCount * blockWidth) / unitSize);
+            int partialBlueBlockIndex1 = (partialRedBlockIndex > 0 ? Math.min((int) Math.floor(value2 / unitSize), partialBlocks - partialRedBlockIndex) : 0);
+            int fullBlueBlockCount = Math.max(0, (int) Math.floor(value2 / blockWidth) - (partialRedBlockIndex > 0 ? 1 : 0));
+            int partialBlueBlockIndex2 = (int) Math.floor((value2 - (fullBlueBlockCount * blockWidth) - (partialBlueBlockIndex1 * unitSize)) / unitSize);
 
             BodyContent bc = getBodyContent();
             String body = bc.getString().trim();
             JspWriter out = bc.getEnclosingWriter();
 
 
+            // Beginning
             if (showA) {
-                out.print(MessageFormat.format(body, new Object[]{fullBlockCount > 0 || partialBlockIndex > 0 ? "a" : "a0"}));
+                String format = "a0";
+                if (fullRedBlockCount > 0 || partialRedBlockIndex > 0) {
+                    format = "a1";
+                } else if (partialBlueBlockIndex1 == 0 && (fullBlueBlockCount > 0 || partialBlueBlockIndex2 > 0)) {
+                    format = "a2";
+                }
+                out.print(MessageFormat.format(body, new Object[]{format}));
             }
 
-            String fullBody = MessageFormat.format(body, new Object[]{new Integer(partialBlocks)});
-            for (int i = 0; i < fullBlockCount; i++) {
-                out.print(fullBody);
+            // Full red blocks
+            String fullRedBody = MessageFormat.format(body, new Object[]{partialBlocks + "+0"});
+            for (int i = 0; i < fullRedBlockCount; i++) {
+                out.print(fullRedBody);
             }
 
-            if (partialBlockIndex > 0) {
-                String partialBody = MessageFormat.format(body, new Object[]{new Integer(partialBlockIndex)});
+            // Mixed red/blue block (mid-block transition)
+            if (partialRedBlockIndex > 0) {
+                String partialBody = MessageFormat.format(body, new Object[]{partialRedBlockIndex + "+" + partialBlueBlockIndex1});
                 out.print(partialBody);
             }
 
-            int emptyBlocks = showEmptyBlocks ? fullBlocks - fullBlockCount - (partialBlockIndex > 0 ? 1 : 0) : 0;
+            // Full blue blocks
+            String fullBlueBody = MessageFormat.format(body, new Object[]{"0+" + partialBlocks});
+            for (int i = 0; i < fullBlueBlockCount; i++) {
+                out.print(fullBlueBody);
+            }
 
+            // Partial blue block
+            if (partialBlueBlockIndex2 > 0) {
+                String partialBody = MessageFormat.format(body, new Object[]{"0+" + partialBlueBlockIndex2});
+                out.print(partialBody);
+            }
+
+            // Empty blocks
+            int emptyBlocks = showEmptyBlocks ? fullBlocks - (fullRedBlockCount + fullBlueBlockCount + (partialRedBlockIndex > 0 ? 1 : 0) + (partialBlueBlockIndex2 > 0 ? 1 : 0)) : 0;
             if (emptyBlocks > 0) {
-                String emptyBody = MessageFormat.format(body, new Object[]{new Integer(0)});
+                String emptyBody = MessageFormat.format(body, new Object[]{"0+0"});
                 for (int i = 0; i < emptyBlocks; i++) {
                     out.print(emptyBody);
                 }
             }
 
+            // End
             if (showB) {
-                out.print(MessageFormat.format(body, new Object[]{fullBlockCount == fullBlocks ? "b" : "b0"}));
+                String format = "b0";
+                if (fullRedBlockCount == fullBlocks) {
+                    format = "b1";
+                } else if (fullRedBlockCount + (partialRedBlockIndex + partialBlueBlockIndex1 == partialBlocks ? 1 : 0) + fullBlueBlockCount == fullBlocks) {
+                    format = "b2";
+                }
+                out.print(MessageFormat.format(body, new Object[]{format}));
             }
-
-
 
         } catch (IOException ioe) {
             throw new JspException("Error:IOException while writing to client" + ioe.getMessage());
@@ -83,6 +114,14 @@ public class VisualScoreTag extends BodyTagSupport {
 
     public void setValue(double value) {
         this.value = value;
+    }
+
+    public double getValue2() {
+        return value2;
+    }
+
+    public void setValue2(double value2) {
+        this.value2 = value2;
     }
 
     public double getMinValue() {
