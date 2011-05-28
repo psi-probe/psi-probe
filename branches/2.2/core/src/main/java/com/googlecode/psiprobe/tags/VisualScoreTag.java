67385578e4af7fa57714c1c169659d4de7d96d42
MALfunction84@gmail.com <MALfunction84@gmail.com@c64a9a66-49c4-11de-acc5-7fb027fc3da9>
2011-05-28 06:15:16 +0000

    Update issue 127 Merging trunk:529,532-533,536 into branches/2.2.

--- branches/2.2/core/src/main/java/com/googlecode/psiprobe/tags/VisualScoreTag.java
+++ branches/2.2/core/src/main/java/com/googlecode/psiprobe/tags/VisualScoreTag.java
@@ -16,8 +16,12 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.BodyContent;
 import javax.servlet.jsp.tagext.BodyTagSupport;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class VisualScoreTag extends BodyTagSupport {
+
+    protected Log log = LogFactory.getLog(getClass());
 
     private double value = 0;
     private double value2 = 0;
@@ -31,76 +35,95 @@ public class VisualScoreTag extends BodyTagSupport {
 
     public int doAfterBody() throws JspException {
 
+        if (value < minValue) {
+            log.info("value " + value + " is less than min value " + minValue);
+            value = minValue;
+        }
+        if (value > maxValue) {
+            log.info("value " + value + " is greater than max value " + maxValue);
+            value = maxValue;
+        }
+        if (value + value2 < minValue || value2 < 0) {
+            log.info("value2 " + value2 + " is less than min value");
+            value2 = 0;
+        }
+        if (value + value2 > maxValue) {
+            log.info("value2 " + value2 + " is greater than max value");
+            value2 = maxValue - value;
+        }
+
+        double unitSize = (maxValue - minValue) / (fullBlocks * partialBlocks);
+        double blockWidth = unitSize * partialBlocks;
+
+        int fullRedBlockCount = (int) Math.floor(value / blockWidth);
+        int partialRedBlockIndex = (int) Math.floor((value - fullRedBlockCount * blockWidth) / unitSize);
+        int partialBlueBlockIndex1 = (partialRedBlockIndex > 0 ? Math.min((int) Math.floor(value2 / unitSize), partialBlocks - partialRedBlockIndex) : 0);
+        int fullBlueBlockCount = Math.max(0, (int) Math.floor(value2 / blockWidth) - (partialRedBlockIndex > 0 ? 1 : 0));
+        int partialBlueBlockIndex2 = (int) Math.floor((value2 - (fullBlueBlockCount * blockWidth) - (partialBlueBlockIndex1 * unitSize)) / unitSize);
+
+        BodyContent bc = getBodyContent();
+        String body = bc.getString().trim();
+
+        StringBuffer buf = new StringBuffer();
+
+        // Beginning
+        if (showA) {
+            String format = "a0";
+            if (fullRedBlockCount > 0 || partialRedBlockIndex > 0) {
+                format = "a1";
+            } else if (partialBlueBlockIndex1 == 0 && (fullBlueBlockCount > 0 || partialBlueBlockIndex2 > 0)) {
+                format = "a2";
+            }
+            buf.append(MessageFormat.format(body, new Object[]{format}));
+        }
+
+        // Full red blocks
+        String fullRedBody = MessageFormat.format(body, new Object[]{partialBlocks + "+0"});
+        for (int i = 0; i < fullRedBlockCount; i++) {
+            buf.append(fullRedBody);
+        }
+
+        // Mixed red/blue block (mid-block transition)
+        if (partialRedBlockIndex > 0) {
+            String partialBody = MessageFormat.format(body, new Object[]{partialRedBlockIndex + "+" + partialBlueBlockIndex1});
+            buf.append(partialBody);
+        }
+
+        // Full blue blocks
+        String fullBlueBody = MessageFormat.format(body, new Object[]{"0+" + partialBlocks});
+        for (int i = 0; i < fullBlueBlockCount; i++) {
+            buf.append(fullBlueBody);
+        }
+
+        // Partial blue block
+        if (partialBlueBlockIndex2 > 0) {
+            String partialBody = MessageFormat.format(body, new Object[]{"0+" + partialBlueBlockIndex2});
+            buf.append(partialBody);
+        }
+
+        // Empty blocks
+        int emptyBlocks = showEmptyBlocks ? fullBlocks - (fullRedBlockCount + fullBlueBlockCount + (partialRedBlockIndex > 0 ? 1 : 0) + (partialBlueBlockIndex2 > 0 ? 1 : 0)) : 0;
+        if (emptyBlocks > 0) {
+            String emptyBody = MessageFormat.format(body, new Object[]{"0+0"});
+            for (int i = 0; i < emptyBlocks; i++) {
+                buf.append(emptyBody);
+            }
+        }
+
+        // End
+        if (showB) {
+            String format = "b0";
+            if (fullRedBlockCount == fullBlocks) {
+                format = "b1";
+            } else if (fullRedBlockCount + (partialRedBlockIndex + partialBlueBlockIndex1 == partialBlocks ? 1 : 0) + fullBlueBlockCount == fullBlocks) {
+                format = "b2";
+            }
+            buf.append(MessageFormat.format(body, new Object[]{format}));
+        }
+
         try {
-            double unitSize = (maxValue - minValue) / (fullBlocks * partialBlocks);
-            double blockWidth = unitSize * partialBlocks;
-
-            int fullRedBlockCount = (int) Math.floor(value / blockWidth);
-            int partialRedBlockIndex = (int) Math.floor((value - fullRedBlockCount * blockWidth) / unitSize);
-            int partialBlueBlockIndex1 = (partialRedBlockIndex > 0 ? Math.min((int) Math.floor(value2 / unitSize), partialBlocks - partialRedBlockIndex) : 0);
-            int fullBlueBlockCount = Math.max(0, (int) Math.floor(value2 / blockWidth) - (partialRedBlockIndex > 0 ? 1 : 0));
-            int partialBlueBlockIndex2 = (int) Math.floor((value2 - (fullBlueBlockCount * blockWidth) - (partialBlueBlockIndex1 * unitSize)) / unitSize);
-
-            BodyContent bc = getBodyContent();
-            String body = bc.getString().trim();
             JspWriter out = bc.getEnclosingWriter();
-
-
-            // Beginning
-            if (showA) {
-                String format = "a0";
-                if (fullRedBlockCount > 0 || partialRedBlockIndex > 0) {
-                    format = "a1";
-                } else if (partialBlueBlockIndex1 == 0 && (fullBlueBlockCount > 0 || partialBlueBlockIndex2 > 0)) {
-                    format = "a2";
-                }
-                out.print(MessageFormat.format(body, new Object[]{format}));
-            }
-
-            // Full red blocks
-            String fullRedBody = MessageFormat.format(body, new Object[]{partialBlocks + "+0"});
-            for (int i = 0; i < fullRedBlockCount; i++) {
-                out.print(fullRedBody);
-            }
-
-            // Mixed red/blue block (mid-block transition)
-            if (partialRedBlockIndex > 0) {
-                String partialBody = MessageFormat.format(body, new Object[]{partialRedBlockIndex + "+" + partialBlueBlockIndex1});
-                out.print(partialBody);
-            }
-
-            // Full blue blocks
-            String fullBlueBody = MessageFormat.format(body, new Object[]{"0+" + partialBlocks});
-            for (int i = 0; i < fullBlueBlockCount; i++) {
-                out.print(fullBlueBody);
-            }
-
-            // Partial blue block
-            if (partialBlueBlockIndex2 > 0) {
-                String partialBody = MessageFormat.format(body, new Object[]{"0+" + partialBlueBlockIndex2});
-                out.print(partialBody);
-            }
-
-            // Empty blocks
-            int emptyBlocks = showEmptyBlocks ? fullBlocks - (fullRedBlockCount + fullBlueBlockCount + (partialRedBlockIndex > 0 ? 1 : 0) + (partialBlueBlockIndex2 > 0 ? 1 : 0)) : 0;
-            if (emptyBlocks > 0) {
-                String emptyBody = MessageFormat.format(body, new Object[]{"0+0"});
-                for (int i = 0; i < emptyBlocks; i++) {
-                    out.print(emptyBody);
-                }
-            }
-
-            // End
-            if (showB) {
-                String format = "b0";
-                if (fullRedBlockCount == fullBlocks) {
-                    format = "b1";
-                } else if (fullRedBlockCount + (partialRedBlockIndex + partialBlueBlockIndex1 == partialBlocks ? 1 : 0) + fullBlueBlockCount == fullBlocks) {
-                    format = "b2";
-                }
-                out.print(MessageFormat.format(body, new Object[]{format}));
-            }
-
+            out.print(buf.toString());
         } catch (IOException ioe) {
             throw new JspException("Error:IOException while writing to client" + ioe.getMessage());
         }
@@ -113,9 +136,6 @@ public class VisualScoreTag extends BodyTagSupport {
     }
 
     public void setValue(double value) {
-        if (value < 0) {
-            return;
-        }
         this.value = value;
     }
 
@@ -124,9 +144,6 @@ public class VisualScoreTag extends BodyTagSupport {
     }
 
     public void setValue2(double value2) {
-        if (value2 < 0) {
-            return;
-        }
         this.value2 = value2;
     }
 
