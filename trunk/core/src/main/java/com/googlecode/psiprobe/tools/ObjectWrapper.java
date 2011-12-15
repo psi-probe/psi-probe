ee7b52d051e1fd90ebef75b1990c6361b97095e5
MALfunction84@gmail.com <MALfunction84@gmail.com@c64a9a66-49c4-11de-acc5-7fb027fc3da9>
2011-12-15 06:52:10 +0000

    Moving test code from main methods into JUnit tests.

--- trunk/core/src/main/java/com/googlecode/psiprobe/tools/ObjectWrapper.java
+++ trunk/core/src/main/java/com/googlecode/psiprobe/tools/ObjectWrapper.java
@@ -40,20 +40,4 @@ class ObjectWrapper {
         return o.hashCode();
     }
 
-    public static void main(String[] args) {
-        Map session = new HashMap();
-        session.put("test1", "test message");
-        List bikes = new ArrayList();
-        bikes.add("specialized");
-        bikes.add("kona");
-        bikes.add("GT");
-        session.put("bikes", bikes);
-
-        Map bikeParts = new TreeMap();
-        bikeParts.put("bikes", bikes);
-        session.put("parts", bikeParts);
-
-        System.out.println(Instruments.sizeOf(session));
-
-    }
 }
