a36f2c8786d620a2073f758af253c35f4053ea0d
Mark Lewis <malfunction84@gmail.com>
2012-03-07 19:05:31 +0000

    - Modifying testLocalhost so it actually tests localhost (127.0.0.1). - Preserving original test as testGoogle.

--- core/src/test/java/com/googlecode/psiprobe/tools/WhoisTests.java
+++ core/src/test/java/com/googlecode/psiprobe/tools/WhoisTests.java
@@ -23,6 +23,20 @@ import junit.framework.TestCase;
 public class WhoisTests extends TestCase {
 
     public void testLocalhost() throws IOException {
+        int a = 127;
+        int b = 0;
+        int c = 0;
+        int d = 1;
+        String dotted = a + "." + b + "." + c + "." + d;
+        byte[] bytes = new byte[] {(byte) a, (byte) b, (byte) c, (byte) d};
+
+        Response response = Whois.lookup("whois.arin.net", 43, "n " + dotted, 5);
+        Assert.assertEquals("SPECIAL-IPV4-LOOPBACK-IANA-RESERVED", response.getData().get("NetName"));
+        //System.out.println(InetAddress.getByName(dotted).getHostName());
+        //System.out.println(InetAddress.getByAddress(bytes).getHostName());
+    }
+    
+    public void testGoogle() throws IOException {
         int a = 74;
         int b = 125;
         int c = 45;
