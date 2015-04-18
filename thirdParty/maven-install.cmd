3c145a53d6d504cdbb3b6e25a54dc497fe46cd9d
Jeremy Landis <jeremylandis@hotmail.com>
2015-04-18 13:53:53 -0400

    Add third party to installation
    For users behind their own nexus repositories, the included repository
    information does not work as expected.  Built this into process better
    and removed the old repos as now unnecessary.

---
+++ thirdParty/maven-install.cmd
@@ -0,0 +1,7 @@
+@echo off
+pushd "%~dp0"
+call mvn install:install-file -Dfile=jmxri-1.2.1.jar -DgroupId=javax.management -DartifactId=jmxri -Dversion=1.2.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=jmxtools-1.2.1.jar -DgroupId=javax.management -DartifactId=jmxtools -Dversion=1.2.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=ojdbc6-11.2.0.4.0.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.4.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=ucp-11.2.0.4.0.jar -DgroupId=com.oracle -DartifactId=ucp -Dversion=11.2.0.4.0 -Dpackaging=jar
+popd
