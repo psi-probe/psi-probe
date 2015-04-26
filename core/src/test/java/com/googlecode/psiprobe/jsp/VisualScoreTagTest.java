76bf520e88fc52c1786d5edd64b77a8bc51cf481
Diogo Sant'Ana <diogosantana@gmail.com>
2015-04-26 05:07:06 -0300

    Fixing unit test VisualScoreTagTest.

--- core/src/test/java/com/googlecode/psiprobe/jsp/VisualScoreTagTest.java
+++ core/src/test/java/com/googlecode/psiprobe/jsp/VisualScoreTagTest.java
@@ -5,11 +5,11 @@ import junit.framework.TestCase;
 public class VisualScoreTagTest extends TestCase {
 
 	public void testRangeScan() {
-		// As usade in appRuntimeInfo.jsp
-		doTestRangeScan(8, 5, true);
+		// As used in appRuntimeInfo.jsp
 		doTestRangeScan(8, 5, false);
+		doTestRangeScan(8, 5, true);
 		
-		/* As usade in memory_pools.jsp, application.jsp, datasourcegroup.jsp,
+		/* As used in memory_pools.jsp, application.jsp, datasourcegroup.jsp,
 		 * datasources_table.jsp, resources.jsp and sysinfo.jsp
 		 */
 		doTestRangeScan(10, 5, false);
@@ -19,6 +19,7 @@ public class VisualScoreTagTest extends TestCase {
 	private void doTestRangeScan(int fullBlocks, int partialBlocks, boolean invertLoopIndexes) {
 		int value = 0;
 		int value2 = 0;
+		int count = 0;
 		for (int i = 0; i <= 100; i++) {
 			for (int j = 0; j <= 100; j++) {
 				if (invertLoopIndexes) {
@@ -30,7 +31,7 @@ public class VisualScoreTagTest extends TestCase {
 				}
 				String[] split = callCalculateSuffix(value, value2, fullBlocks, partialBlocks);
 				for (int k = 0; k < split.length; k++) {
-					 System.out.println(split[k]);
+					// System.out.println(split[k]);
 					String[] values = split[k].split("\\+");
 	
 					if (values.length > 1) {
@@ -38,22 +39,26 @@ public class VisualScoreTagTest extends TestCase {
 						value2 = Integer.valueOf(values[1]).intValue();
 	
 						if (value > 5 || value2 > 5) {
+							count++;
 							StringBuffer msg = new StringBuffer();
 							msg.append("Found incorrect value ");
 							msg.append(split[k]);
 							msg.append(". value = ");
-							msg.append(value);
+							msg.append(invertLoopIndexes ? j : i);
 							msg.append(" value2 = ");
-							msg.append(value2);
+							msg.append(invertLoopIndexes ? i : j);
 							msg.append(" fullBlocks = ");
 							msg.append(fullBlocks);
 							msg.append(" partialBlocks = ");
 							msg.append(partialBlocks);
-							fail(msg.toString());
+							// System.out.println(msg.toString());
 						}
 					}
 				}
 			}
+		}
+		if (count > 0) {
+			fail("Incorrect values were founded " + count + " times");
 		}
 	}
 
@@ -61,7 +66,7 @@ public class VisualScoreTagTest extends TestCase {
 		String body = "{0} ";
 
 		VisualScoreTag visualScoreTag = new VisualScoreTag();
-		
+			
 		// Values are based on datasources_table.jsp 
 		visualScoreTag.setFullBlocks(fullBlocks);
 		visualScoreTag.setPartialBlocks(partialBlocks);
