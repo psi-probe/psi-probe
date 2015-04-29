98b3b17fc00d1db64908558853fca73e28b46337
Diogo Sant'Ana <diogosantana@gmail.com>
2015-04-28 21:31:51 -0300

    Fixed #466

--- core/src/main/java/com/googlecode/psiprobe/jsp/VisualScoreTag.java
+++ core/src/main/java/com/googlecode/psiprobe/jsp/VisualScoreTag.java
@@ -26,20 +26,66 @@ import org.apache.commons.logging.LogFactory;
  */
 public class VisualScoreTag extends BodyTagSupport {
 
+    private static final long serialVersionUID = -5653846466205838602L;
+
+    private static final String WHITE_LEFT_BORDER = "a0";
+    private static final String RED_LEFT_BORDER = "a1";
+    private static final String BLUE_LEFT_BORDER = "a2";
+
+    private static final String WHITE_RIGHT_BORDER = "b0";
+    private static final String RED_RIGHT_BORDER = "b1";
+    private static final String BLUE_RIGHT_BORDER = "b2";
+
     protected Log log = LogFactory.getLog(getClass());
 
+    /**
+     * Red value.
+     */
     private double value = 0;
+
+    /**
+     * Blue value.
+     */
     private double value2 = 0;
+
     private double minValue = 0;
     private double maxValue = 100;
+
+    /**
+     * Number of parts in one block.<br>
+     * It always must be 5 to match the 5 different gifs available at
+     * /src/main/webapp/css/classic/gifs
+     */
     private int partialBlocks = 1;
+
+    /**
+     * Total number of blocks.<br>
+     * fullBlocks + 2 img elements will be added to the page.<br>
+     * The plus 2 is due the left and right border.
+     */
     private int fullBlocks = 5;
+
     private boolean showEmptyBlocks=false;
     private boolean showA = false;
     private boolean showB = false;
 
     public int doAfterBody() throws JspException {
+        BodyContent bc = getBodyContent();
+        String body = bc.getString().trim();
 
+        StringBuffer buf = calculateSuffix(body);
+
+        try {
+            JspWriter out = bc.getEnclosingWriter();
+            out.print(buf.toString());
+        } catch (IOException ioe) {
+            throw new JspException("Error:IOException while writing to client" + ioe.getMessage());
+        }
+
+        return SKIP_BODY;
+    }
+
+    StringBuffer calculateSuffix(String body) {
         if (value < minValue) {
             log.info("value " + value + " is less than min value " + minValue);
             value = minValue;
@@ -63,21 +109,18 @@ public class VisualScoreTag extends BodyTagSupport {
         int fullRedBlockCount = (int) Math.floor(value / blockWidth);
         int partialRedBlockIndex = (int) Math.floor((value - fullRedBlockCount * blockWidth) / unitSize);
         int partialBlueBlockIndex1 = (partialRedBlockIndex > 0 ? Math.min((int) Math.floor(value2 / unitSize), partialBlocks - partialRedBlockIndex) : 0);
-        int fullBlueBlockCount = Math.max(0, (int) Math.floor(value2 / blockWidth) - (partialRedBlockIndex > 0 ? 1 : 0));
+        int fullBlueBlockCount = Math.max(0, (int) Math.ceil(value2 / blockWidth) - (partialRedBlockIndex > 0 ? 1 : 0));
         int partialBlueBlockIndex2 = (int) Math.floor((value2 - (fullBlueBlockCount * blockWidth) - (partialBlueBlockIndex1 * unitSize)) / unitSize);
-
-        BodyContent bc = getBodyContent();
-        String body = bc.getString().trim();
 
         StringBuffer buf = new StringBuffer();
 
         // Beginning
         if (showA) {
-            String format = "a0";
+            String format = WHITE_LEFT_BORDER;
             if (fullRedBlockCount > 0 || partialRedBlockIndex > 0) {
-                format = "a1";
+                format = RED_LEFT_BORDER;
             } else if (partialBlueBlockIndex1 == 0 && (fullBlueBlockCount > 0 || partialBlueBlockIndex2 > 0)) {
-                format = "a2";
+                format = BLUE_LEFT_BORDER;
             }
             buf.append(MessageFormat.format(body, new Object[]{format}));
         }
@@ -117,23 +160,16 @@ public class VisualScoreTag extends BodyTagSupport {
 
         // End
         if (showB) {
-            String format = "b0";
+            String format = WHITE_RIGHT_BORDER;
             if (fullRedBlockCount == fullBlocks) {
-                format = "b1";
+                format = RED_RIGHT_BORDER;
             } else if (fullRedBlockCount + (partialRedBlockIndex + partialBlueBlockIndex1 == partialBlocks ? 1 : 0) + fullBlueBlockCount == fullBlocks) {
-                format = "b2";
+                format = BLUE_RIGHT_BORDER;
             }
-            buf.append(MessageFormat.format(body, new Object[]{format}));
+            buf.append(MessageFormat.format(body, new Object[] { format }));
         }
 
-        try {
-            JspWriter out = bc.getEnclosingWriter();
-            out.print(buf.toString());
-        } catch (IOException ioe) {
-            throw new JspException("Error:IOException while writing to client" + ioe.getMessage());
-        }
-
-        return SKIP_BODY;
+        return buf;
     }
 
     public double getValue() {
