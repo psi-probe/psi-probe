27745a43f73f8893ddc08d6d2723b74dbdcb8c27
Jeremy Landis <jeremylandis@hotmail.com>
2016-01-18 17:39:20 -0500

    ParseInt instead of using valueOf

--- core/src/test/java/com/googlecode/psiprobe/jsp/VisualScoreTagTest.java
+++ core/src/test/java/com/googlecode/psiprobe/jsp/VisualScoreTagTest.java
@@ -48,8 +48,8 @@ public class VisualScoreTagTest extends TestCase {
           // System.out.println(split[k]);
           String[] values = suffix.split("\\+");
           if (values.length > 1) {
-            value = Integer.valueOf(values[0]);
-            value2 = Integer.valueOf(values[1]);
+            value = Integer.parseInt(values[0]);
+            value2 = Integer.parseInt(values[1]);
             if (value > 5 || value2 > 5) {
               count++;
               StringBuffer msg = new StringBuffer();
