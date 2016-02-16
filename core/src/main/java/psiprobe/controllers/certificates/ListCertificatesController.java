4f1755998bd71c4cbfc77f58e6848ad63d4f6889
Diogo Sant'Ana <diogosantana@gmail.com>
2016-02-15 22:30:11 -0200

    Certificates - exception handling

--- core/src/main/java/psiprobe/controllers/certificates/ListCertificatesController.java
+++ core/src/main/java/psiprobe/controllers/certificates/ListCertificatesController.java
@@ -2,15 +2,10 @@ package psiprobe.controllers.certificates;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -24,6 +19,8 @@ import org.apache.catalina.connector.Connector;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.coyote.ProtocolHandler;
 import org.apache.coyote.http11.AbstractHttp11JsseProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 
 import psiprobe.controllers.TomcatContainerController;
@@ -32,110 +29,105 @@ import psiprobe.model.certificates.ConnectorInfo;
 
 public class ListCertificatesController extends TomcatContainerController {
 
+  /** The Constant Logger. */
+  private static final Logger logger = LoggerFactory.getLogger(ListCertificatesController.class);
+
   @Override
   protected ModelAndView handleRequestInternal(HttpServletRequest request,
       HttpServletResponse response) throws Exception {
 
     ModelAndView modelAndView = new ModelAndView(getViewName());
 
-    // Acquiring System configured Key Store
-    String keyStore = System.getProperty("javax.net.ssl.keyStore");
-    if (keyStore != null && !"".equals(keyStore)) {
-      File trustStoreFile = new File(keyStore);
-      if (trustStoreFile.exists()) {
-        String keyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword");
-        String keyStoreType = System.getProperty("javax.net.ssl.keyStoreType");
-        List<Cert> certs = getCertificates(keyStoreType, trustStoreFile, keyStorePassword);
-        modelAndView.addObject("systemKeyCerts", certs);
-      }
-    }
-
-    // Acquiring System configured Trust Store
-    String trustStore = System.getProperty("javax.net.ssl.trustStore");
-    if (trustStore != null && !"".equals(trustStore)) {
-      File trustStoreFile = new File(trustStore);
-      if (trustStoreFile.exists()) {
-        String trustStorePassword = System.getProperty("javax.net.ssl.trustStorePassword");
-        String trustStoreType = System.getProperty("javax.net.ssl.trustStoreType");
-        List<Cert> certs = getCertificates(trustStoreType, trustStoreFile, trustStorePassword);
-        modelAndView.addObject("systemTrustCerts", certs);
-      }
-    }
-
-    List<Connector> connectors = getContainerWrapper().getTomcatContainer().findConnectors();
-    // TODO diogo exception handle
-    List<ConnectorInfo> infos = getConnectorInfos(connectors);
-
-    for (ConnectorInfo info : infos) {
-      if (info.getKeyStoreFile() != null) {
-        List<Cert> certs =
-            getCertificates(info.getKeystoreType(), info.getKeyStoreFile(), info.getKeystorePass());
-        info.setKeyStoreCerts(certs);
-      }
-      if (info.getTrustStoreFile() != null) {
-        List<Cert> certs = getCertificates(info.getTruststoreType(), info.getTrustStoreFile(),
-            info.getTruststorePass());
-        info.setTrustStoreCerts(certs);
-      }
-    }
-
-    return modelAndView.addObject("connectors", infos);
-  }
-
-  public List<Cert> getCertificates(String storeType, File storeFile, String storePassword) {
     try {
-      List<Cert> certs = new ArrayList<>();
-      KeyStore keyStore;
-
-      // Get key store
-      if (storeType != null) {
-        keyStore = KeyStore.getInstance(storeType);
-      } else {
-        keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-      }
-
-      // Get password
-      char[] password = null;
-      if (storePassword != null) {
-        password = storePassword.toCharArray();
-      }
-
-      // Load key store from file
-      try (InputStream storeInput = new FileInputStream(storeFile)) {
-        keyStore.load(storeInput, password);
-      }
-
-      Enumeration<String> keystoreAliases = keyStore.aliases();
-      while (keystoreAliases.hasMoreElements()) {
-        String alias = keystoreAliases.nextElement();
-
-        Certificate[] certificateChain = keyStore.getCertificateChain(alias);
-
-        if (certificateChain != null) {
-          for (int i = 0; i < certificateChain.length; i++) {
-            X509Certificate x509Cert = (X509Certificate) certificateChain[i];
-            addToStore(certs, alias, x509Cert);
-          }
-        } else {
-          X509Certificate x509Cert = (X509Certificate) keyStore.getCertificate(alias);
-          addToStore(certs, alias, x509Cert);
+      // Acquiring System configured Key Store
+      String keyStore = System.getProperty("javax.net.ssl.keyStore");
+      if (keyStore != null && !"".equals(keyStore)) {
+        File trustStoreFile = new File(keyStore);
+        if (trustStoreFile.exists()) {
+          String keyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword");
+          String keyStoreType = System.getProperty("javax.net.ssl.keyStoreType");
+          List<Cert> certs = getCertificates(keyStoreType, trustStoreFile, keyStorePassword);
+          modelAndView.addObject("systemKeyCerts", certs);
         }
       }
 
-      return certs;
+      // Acquiring System configured Trust Store
+      String trustStore = System.getProperty("javax.net.ssl.trustStore");
+      if (trustStore != null && !"".equals(trustStore)) {
+        File trustStoreFile = new File(trustStore);
+        if (trustStoreFile.exists()) {
+          String trustStorePassword = System.getProperty("javax.net.ssl.trustStorePassword");
+          String trustStoreType = System.getProperty("javax.net.ssl.trustStoreType");
+          List<Cert> certs = getCertificates(trustStoreType, trustStoreFile, trustStorePassword);
+          modelAndView.addObject("systemTrustCerts", certs);
+        }
+      }
 
-    } catch (KeyStoreException e) {
-      e.printStackTrace();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-    } catch (CertificateException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
+      List<Connector> connectors = getContainerWrapper().getTomcatContainer().findConnectors();
+      List<ConnectorInfo> infos = getConnectorInfos(connectors);
+
+      for (ConnectorInfo info : infos) {
+        if (info.getKeyStoreFile() != null) {
+          List<Cert> certs = getCertificates(info.getKeystoreType(), info.getKeyStoreFile(),
+              info.getKeystorePass());
+          info.setKeyStoreCerts(certs);
+        }
+        if (info.getTrustStoreFile() != null) {
+          List<Cert> certs = getCertificates(info.getTruststoreType(), info.getTrustStoreFile(),
+              info.getTruststorePass());
+          info.setTrustStoreCerts(certs);
+        }
+      }
+
+      modelAndView.addObject("connectors", infos);
+    } catch (Exception e) {
+      logger.error("There was an exception listing certificates ", e);
     }
-    return null;
+
+    return modelAndView;
+  }
+
+  public List<Cert> getCertificates(String storeType, File storeFile, String storePassword)
+      throws Exception {
+    KeyStore keyStore;
+
+    // Get key store
+    if (storeType != null) {
+      keyStore = KeyStore.getInstance(storeType);
+    } else {
+      keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    }
+
+    // Get password
+    char[] password = null;
+    if (storePassword != null) {
+      password = storePassword.toCharArray();
+    }
+
+    // Load key store from file
+    try (InputStream storeInput = new FileInputStream(storeFile)) {
+      keyStore.load(storeInput, password);
+    }
+
+    List<Cert> certs = new ArrayList<>();
+
+    Enumeration<String> keystoreAliases = keyStore.aliases();
+    while (keystoreAliases.hasMoreElements()) {
+      String alias = keystoreAliases.nextElement();
+
+      Certificate[] certificateChain = keyStore.getCertificateChain(alias);
+
+      if (certificateChain != null) {
+        for (int i = 0; i < certificateChain.length; i++) {
+          X509Certificate x509Cert = (X509Certificate) certificateChain[i];
+          addToStore(certs, alias, x509Cert);
+        }
+      } else {
+        X509Certificate x509Cert = (X509Certificate) keyStore.getCertificate(alias);
+        addToStore(certs, alias, x509Cert);
+      }
+    }
+    return certs;
   }
 
   private List<ConnectorInfo> getConnectorInfos(List<Connector> connectors)
@@ -153,7 +145,6 @@ public class ListCertificatesController extends TomcatContainerController {
         if (!protocol.getSecure()) {
           continue;
         }
-        // TODO diogo exception handle
         infos.add(toConnectorInfo(protocol));
       }
     }
@@ -173,6 +164,7 @@ public class ListCertificatesController extends TomcatContainerController {
         info.setKeyStoreFile(file);
       }
     }
+
     String truststoreFile = info.getTruststoreFile();
     if (truststoreFile != null && !"".equals(truststoreFile)) {
       File file = new File(truststoreFile);
