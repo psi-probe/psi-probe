c67fbc4873affa684c54ae7b7a95e2ff4119dae0
Diogo Sant'Ana <diogosantana@gmail.com>
2016-02-16 01:23:27 -0200

    Unit test exception in Utils.closeStream() as sonar tip

--- core/src/test/java/psiprobe/UtilsTest.java
+++ core/src/test/java/psiprobe/UtilsTest.java
@@ -1,10 +1,12 @@
 package psiprobe;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import mockit.Expectations;
 import mockit.Mocked;
 import mockit.Verifications;
 import mockit.integration.junit4.JMockit;
@@ -14,6 +16,25 @@ public class UtilsTest {
 
   @Test
   public void testCloseStream(@Mocked final Closeable stream) throws Exception {
+    Utils.closeStream(stream);
+
+    new Verifications() {
+      {
+        stream.close();
+        times = 1;
+      }
+    };
+  };
+
+  @Test
+  public void testCloseStreamException(@Mocked final Closeable stream) throws Exception {
+    new Expectations() {
+      {
+        stream.close();
+        result = new IOException();
+      }
+    };
+
     Utils.closeStream(stream);
 
     new Verifications() {
