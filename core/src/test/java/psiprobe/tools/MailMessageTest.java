a14a466692752c897f47c5be4d248687c0b0c8ff
Jeremy Landis <jeremylandis@hotmail.com>
2016-02-21 15:25:25 -0500

    Switch to javabeanTester 1.4.1 and remove ignores

--- core/src/test/java/psiprobe/tools/MailMessageTest.java
+++ core/src/test/java/psiprobe/tools/MailMessageTest.java
@@ -1,6 +1,5 @@
 package psiprobe.tools;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.codebox.bean.JavaBeanTester;
@@ -15,8 +14,6 @@ public class MailMessageTest {
   /**
    * Javabean tester.
    */
-  // TODO 1/25/16 Waiting on fix in JavaBeanTester due to no no-arg constructor
-  @Ignore
   @Test
   public void javabeanTester() {
     JavaBeanTester.builder(MailMessage.class).loadData().test();
