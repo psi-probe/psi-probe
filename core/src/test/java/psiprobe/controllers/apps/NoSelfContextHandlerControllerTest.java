8c421743207bd97063a352f13d714ba7560b1ae7
Jeremy Landis <jeremylandis@hotmail.com>
2016-02-21 15:25:46 -0500

    Add test implementation, fix ignored test, and removed ignore

--- core/src/test/java/psiprobe/controllers/apps/NoSelfContextHandlerControllerTest.java
+++ core/src/test/java/psiprobe/controllers/apps/NoSelfContextHandlerControllerTest.java
@@ -1,11 +1,8 @@
 package psiprobe.controllers.apps;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.codebox.bean.JavaBeanTester;
-
-import psiprobe.controllers.apps.NoSelfContextHandlerController;
 
 /**
  * The Class NoSelfContextHandlerControllerTest.
@@ -15,10 +12,9 @@ public class NoSelfContextHandlerControllerTest {
   /**
    * Javabean tester.
    */
-  @Ignore
   @Test
   public void javabeanTester() {
-    JavaBeanTester.builder(NoSelfContextHandlerController.class).skip("applicationContext", "supportedMethods").test();
+    JavaBeanTester.builder(NoSelfContextHandlerControllerImpl.class).skip("applicationContext", "supportedMethods").test();
   }
 
 }
