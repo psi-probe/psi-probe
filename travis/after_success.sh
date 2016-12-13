43cd2f24d95d714e7730af932aeb138095c4ad78
Jeremy Landis <jeremylandis@hotmail.com>
2016-12-12 20:19:12 -0500

    [travis] Cleanup travis after success script

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -10,18 +10,9 @@
 # PURPOSE.
 #
 
-
-# Get Project Repo
-upstream_repo=$(git config --get remote.origin.url 2>&1)
-echo "Repo detected: ${upstream_repo}"
- 
-# Get the Java version.
-# Java 1.5 will give 15.
-# Java 1.6 will give 16.
-# Java 1.7 will give 17.
-# Java 1.8 will give 18.
-VER=`java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q'`
-echo "Java detected: ${VER}"
+# Get Commit Message
+commit_message=$(git log --format=%B -n 1)
+echo "Current commit detected: ${commit_message}"
 
 # We build for several JDKs on Travis.
 # Some actions, like analyzing the code (Coveralls) and uploading
@@ -35,26 +26,31 @@ echo "Java detected: ${VER}"
 # 3. Deploy site
 #    a. Use -q option to only display Maven errors and warnings.
 
-if [ "$upstream_repo" == "https://github.com/psi-probe/psi-probe.git" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  if [ $VER == "18" ]; then
-    # Send snapshots to sonatype
-    mvn clean deploy -q --settings ./travis/settings.xml
+if [ "$upstream_repo" == "psi-probe/psi-probe" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [[ "$commit_message" != *"[maven-release-plugin]"* ]]; then
+
+  if [ $TRAVIS_JDK_VERSION == "oraclejdk8" ]; then
+    # Deploy sonatype
+    mvn deploy -q --settings ./travis/settings.xml
     echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
 
 	# Send coverage to coveralls
-    mvn clean test jacoco:report coveralls:report -q
+    mvn test jacoco:report coveralls:report -q
     echo -e "Successfully ran coveralls under Travis job ${TRAVIS_JOB_NUMBER}"
 
-	# various issues exist currently in building this so comment for now
+    # Deploy to site
+    # Cannot currently run site this way
 	# mvn site site:deploy -q
 	# echo -e "Successfully deploy site under Travis job ${TRAVIS_JOB_NUMBER}"
   fi
-elif [ "$upstream_repo" == "https://github.com/psi-probe/psi-probe.git" ] && [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
-  if [ $VER == "18" ]; then
+
+elif [ "$upstream_repo" == "psi-probe/psi-probe" ] && [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+
+  if [ $TRAVIS_JDK_VERSION == "oraclejdk8" ]; then
 	# Send coverage to coveralls
 	mvn clean jacoco:report coveralls:report -q
 	echo -e "Successfully ran coveralls under Travis job ${TRAVIS_JOB_NUMBER}"
   fi
+
 else
   echo "Travis build skipped"
 fi
