9b38d6d7513e85632b951bd0e287f1d8a1de3467
Jeremy Landis <jeremylandis@hotmail.com>
2016-12-12 20:10:04 -0500

    [ci] Restructure StartContextController in prep for spring annotations.

---
+++ core/src/main/java/psiprobe/controllers/apps/BaseStartContextController.java
@@ -0,0 +1,37 @@
+/**
+ * Licensed under the GPL License. You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ */
+package psiprobe.controllers.apps;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * Starts a web application.
+ */
+public class BaseStartContextController extends AbstractNoSelfContextHandlerController {
+
+  /** The Constant logger. */
+  private static final Logger logger = LoggerFactory.getLogger(BaseStartContextController.class);
+
+  @Override
+  protected void executeAction(String contextName) throws Exception {
+    getContainerWrapper().getTomcatContainer().start(contextName);
+
+    // Logging action
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    // get username logger
+    String name = auth.getName();
+    logger.info(getMessageSourceAccessor().getMessage("probe.src.log.start"), name, contextName);
+  }
+
+}
--- core/src/test/java/psiprobe/controllers/apps/StartContextControllerTest.java
+++ core/src/test/java/psiprobe/controllers/apps/StartContextControllerTest.java
@@ -28,4 +28,13 @@ public class StartContextControllerTest {
         .skip("applicationContext", "supportedMethods").test();
   }
 
+  /**
+   * Javabean tester summary.
+   */
+  @Test
+  public void javabeanTesterSummary() {
+    JavaBeanTester.builder(StartSummaryContextController.class)
+        .skip("applicationContext", "supportedMethods").test();
+  }
+
 }
