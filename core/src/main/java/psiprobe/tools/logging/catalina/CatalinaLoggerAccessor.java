141ba75fbcd5ca82d46bc22aeb14aba3792e5df4
Jeremy Landis <jeremylandis@hotmail.com>
2017-01-30 23:56:25 -0500

    [ci] Avoid casting possible null dereference

--- core/src/main/java/psiprobe/tools/logging/catalina/CatalinaLoggerAccessor.java
+++ core/src/main/java/psiprobe/tools/logging/catalina/CatalinaLoggerAccessor.java
@@ -42,7 +42,7 @@ public class CatalinaLoggerAccessor extends AbstractLogDestination {
     String dir = (String) invokeMethod(getTarget(), "getDirectory", null, null);
     String prefix = (String) invokeMethod(getTarget(), "getPrefix", null, null);
     String suffix = (String) invokeMethod(getTarget(), "getSuffix", null, null);
-    boolean timestamp = (Boolean) Instruments.getField(getTarget(), "timestamp");
+    boolean timestamp = Boolean.valueOf(String.valueOf(Instruments.getField(getTarget(), "timestamp"))).booleanValue();
     String date = timestamp ? new SimpleDateFormat("yyyy-MM-dd").format(new Date()) : "";
 
     File file = date != null && dir != null && prefix != null && suffix != null
