5aab48d2e38ba4536393bd711407aa2f6736d89d
Jeremy Landis <jeremylandis@hotmail.com>
2017-07-02 20:05:58 -0400

    [depr] Cleanup deprecated code moved from commons lang3 to commons text

--- core/src/main/java/psiprobe/jsp/OutTag.java
+++ core/src/main/java/psiprobe/jsp/OutTag.java
@@ -16,7 +16,7 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 /**
  * The Class OutTag.
