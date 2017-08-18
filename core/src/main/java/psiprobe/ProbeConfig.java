ddea33f70938df83220d496729a87bd4e0ecf143
dougwm <31012277+dougwm@users.noreply.github.com>
2017-08-17 22:25:23 -0400

    add tomee datasourceAccessor issue #1097  (#1098) (#1099)
    fixes issue #1097

--- core/src/main/java/psiprobe/ProbeConfig.java
+++ core/src/main/java/psiprobe/ProbeConfig.java
@@ -159,6 +159,7 @@ public class ProbeConfig extends WebMvcConfigurerAdapter {
     list.add("psiprobe.beans.accessors.OpenEjbManagedDatasourceAccessor");
     list.add("psiprobe.beans.accessors.ViburCpDatasourceAccessor");
     list.add("psiprobe.beans.accessors.TomEEJdbcPoolDatasourceAccessor");
+    list.add("psiprobe.beans.accessors.OpenEjbBasicDatasourceAccessor");
     return list;
   }
 
