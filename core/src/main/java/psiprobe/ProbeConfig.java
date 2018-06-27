bd3aa74219607562254e045170078f4047877956
Jeremy Landis <jeremylandis@hotmail.com>
2018-06-26 22:58:25 -0400

    [ci] Correct formatting of project

--- core/src/main/java/psiprobe/ProbeConfig.java
+++ core/src/main/java/psiprobe/ProbeConfig.java
@@ -230,11 +230,11 @@ public class ProbeConfig implements WebMvcConfigurer {
     logger.debug("Instantiated stdoutFiles");
     List<String> list = new ArrayList<>();
     try {
-        for (Entry<Object, Object> entry : stdout().getObject().entrySet()) {
-            list.add((String) entry.getValue());
-        }
+      for (Entry<Object, Object> entry : stdout().getObject().entrySet()) {
+        list.add((String) entry.getValue());
+      }
     } catch (Exception e) {
-        logger.error("", e);
+      logger.error("", e);
     }
     return list;
   }
