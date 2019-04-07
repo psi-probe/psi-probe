199a862467a0c812551d6e7c465aad79684cc91c
Jeremy Landis <jeremylandis@hotmail.com>
2019-04-07 19:32:11 -0400

    [tests] Start building out more

---
+++ core/src/test/java/psiprobe/ProbeConfigTest.java
@@ -0,0 +1,38 @@
+/**
+ * Licensed under the GPL License. You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ */
+package psiprobe;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ContextConfiguration(classes = ProbeConfig.class, loader=AnnotationConfigWebContextLoader.class)
+@WebAppConfiguration("file:src/test/webapp")
+@ExtendWith(SpringExtension.class)
+public class ProbeConfigTest {
+
+  @Inject
+  private List<String> datasourceMappers;
+
+  @Test
+  public void checkListenerTest() {
+    Assertions.assertNotNull(datasourceMappers);
+  }
+
+}
