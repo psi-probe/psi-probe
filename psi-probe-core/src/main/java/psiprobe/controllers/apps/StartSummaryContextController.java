5310d38f48f097a9c3f398b29387e2468c98731a
Pernoud Aurelien <pernouda@ds.givaudan.com>
2020-07-08 10:02:04 +0200

    Update StartSummaryContextController.java

--- psi-probe-core/src/main/java/psiprobe/controllers/apps/StartSummaryContextController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/apps/StartSummaryContextController.java
@@ -26,6 +26,7 @@ public class StartSummaryContextController extends BaseStartContextController {
 
   @RequestMapping(path = "/app/start_summary.htm")
   @Override
+  public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
     return super.handleRequest(request, response);
   }
 
