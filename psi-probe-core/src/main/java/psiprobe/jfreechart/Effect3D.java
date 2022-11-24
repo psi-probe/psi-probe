8f4d29538ec720a14a59f47695b4e8aced23b318
Jeremy Landis <jeremylandis@hotmail.com>
2022-11-23 19:34:35 -0500

    [ci] Remove public from interface methods

--- psi-probe-core/src/main/java/psiprobe/jfreechart/Effect3D.java
+++ psi-probe-core/src/main/java/psiprobe/jfreechart/Effect3D.java
@@ -64,12 +64,12 @@ public interface Effect3D {
    *
    * @return The offset.
    */
-  public double getXOffset();
+  double getXOffset();
 
   /**
    * Returns the y-offset (in Java2D units) for the 3D effect.
    *
    * @return The offset.
    */
-  public double getYOffset();
+  double getYOffset();
 }
