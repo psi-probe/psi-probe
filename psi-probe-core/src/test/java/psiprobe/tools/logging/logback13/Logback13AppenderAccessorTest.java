41c6c3eadb989b8c8238f9ec36a96585243fc0cd
Jeremy Landis <jeremylandis@hotmail.com>
2022-11-25 16:20:44 -0500

    [tests] Add basic tests for logback 1.3 classes

---
+++ psi-probe-core/src/test/java/psiprobe/tools/logging/logback13/Logback13AppenderAccessorTest.java
@@ -0,0 +1,30 @@
+/*
+ * Licensed under the GPL License. You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ */
+package psiprobe.tools.logging.logback13;
+
+import com.codebox.bean.JavaBeanTester;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * The Class LogbackAppenderAccessorTest.
+ */
+class Logback13AppenderAccessorTest {
+
+  /**
+   * Javabean tester.
+   */
+  @Test
+  void javabeanTester() {
+    JavaBeanTester.builder(Logback13AppenderAccessor.class).skip("level").test();
+  }
+
+}
