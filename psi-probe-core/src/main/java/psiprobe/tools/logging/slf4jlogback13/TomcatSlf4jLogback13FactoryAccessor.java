ad70f048016c5352d1194118bdb30edb5ae9b2d9
Jeremy Landis <jeremylandis@hotmail.com>
2022-11-25 16:07:15 -0500

    [logging] Add support for logback 1.3 variations of logback and tomcat slf4j logback
    - additionally fixes a minor bug in 'if' statement as check was wrong in checking logIndex for  tomcat slf4j logback.
    - add some missing javadocs in log resolver bean

---
+++ psi-probe-core/src/main/java/psiprobe/tools/logging/slf4jlogback13/TomcatSlf4jLogback13FactoryAccessor.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed under the GPL License. You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ */
+package psiprobe.tools.logging.slf4jlogback13;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.reflect.MethodUtils;
+
+import psiprobe.tools.logging.DefaultAccessor;
+
+/**
+ * Wraps a TomcatSlf4jLogback logger factory from a given web application class loader.
+ *
+ * <p>
+ * All TomcatSlf4jLogback classes are loaded via the given class loader and not via psi-probe's own
+ * class loader. For this reasons, all methods on TomcatSlf4jLogback objects are invoked via
+ * reflection.
+ * </p>
+ * <p>
+ * This way, we can even handle different versions of TomcatSlf4jLogback embedded in different WARs.
+ * </p>
+ */
+public class TomcatSlf4jLogback13FactoryAccessor extends DefaultAccessor {
+
+  /**
+   * Attempts to initialize a TomcatSlf4jLogback logger factory via the given class loader.
+   *
+   * @param cl the ClassLoader to use when fetching the factory
+   * @throws ClassNotFoundException the class not found exception
+   * @throws IllegalAccessException the illegal access exception
+   * @throws InvocationTargetException the invocation target exception
+   * @throws NoSuchMethodException the no such method exception
+   * @throws SecurityException the security exception
+   * @throws IllegalArgumentException the illegal argument exception
+   */
+  public TomcatSlf4jLogback13FactoryAccessor(ClassLoader cl)
+      throws ClassNotFoundException, IllegalAccessException, InvocationTargetException,
+      NoSuchMethodException, SecurityException, IllegalArgumentException {
+
+    // Get the SLF4J provider binding, which may or may not be Logback, depending on the binding.
+    final List<?> providers = findServiceProviders(cl);
+    if (providers.isEmpty()) {
+      throw new RuntimeException("The SLF4J provider binding was not Logback");
+    }
+
+    // Get the service provider
+    Object provider = providers.get(0);
+
+    // Initialize the service provider
+    Method initialize = MethodUtils.getAccessibleMethod(provider.getClass(), "initialize");
+    initialize.invoke(provider);
+
+    // Call the logger factory
+    Method getLoggerFactory =
+        MethodUtils.getAccessibleMethod(provider.getClass(), "getLoggerFactory");
+    Object loggerFactory = getLoggerFactory.invoke(provider);
+
+    // Check if the binding is indeed Logback
+    Class<?> loggerFactoryClass =
+        cl.loadClass("org.apache.juli.logging.ch.qos.logback.classic.LoggerContext");
+    if (!loggerFactoryClass.isInstance(loggerFactory)) {
+      throw new RuntimeException("The SLF4J provider binding was not Logback");
+    }
+    setTarget(loggerFactory);
+  }
+
+  /**
+   * Returns the TomcatSlf4jLogback root logger.
+   *
+   * @return the root logger
+   */
+  public TomcatSlf4jLogback13LoggerAccessor getRootLogger() {
+    /*
+     * TomcatSlf4jLogback has no dedicated getRootLogger() method, so we simply access the root
+     * logger by its well-defined name.
+     */
+    return getLogger("ROOT");
+  }
+
+  /**
+   * Returns the TomcatSlf4jLogback logger with a given name.
+   *
+   * @param name the name
+   * @return the Logger with the given name
+   */
+  public TomcatSlf4jLogback13LoggerAccessor getLogger(String name) {
+    try {
+      Class<? extends Object> clazz = getTarget().getClass();
+      Method getLogger = MethodUtils.getAccessibleMethod(clazz, "getLogger", String.class);
+
+      Object logger = getLogger.invoke(getTarget(), name);
+      if (logger == null) {
+        throw new NullPointerException(getTarget() + ".getLogger(\"" + name + "\") returned null");
+      }
+      TomcatSlf4jLogback13LoggerAccessor accessor = new TomcatSlf4jLogback13LoggerAccessor();
+      accessor.setTarget(logger);
+      accessor.setApplication(getApplication());
+      return accessor;
+
+    } catch (Exception e) {
+      logger.error("{}.getLogger('{}') failed", getTarget(), name, e);
+    }
+    return null;
+  }
+
+  /**
+   * Returns a list of wrappers for all TomcatSlf4jLogback appenders that have an associated logger.
+   *
+   * @return a list of {@link TomcatSlf4jLogbackAppenderAccessor}s representing all appenders that
+   *         are in use
+   */
+  @SuppressWarnings("unchecked")
+  public List<TomcatSlf4jLogback13AppenderAccessor> getAppenders() {
+    List<TomcatSlf4jLogback13AppenderAccessor> appenders = new ArrayList<>();
+    try {
+      Class<? extends Object> clazz = getTarget().getClass();
+      Method getLoggerList = MethodUtils.getAccessibleMethod(clazz, "getLoggerList");
+
+      List<Object> loggers = (List<Object>) getLoggerList.invoke(getTarget());
+      for (Object logger : loggers) {
+        TomcatSlf4jLogback13LoggerAccessor accessor = new TomcatSlf4jLogback13LoggerAccessor();
+        accessor.setTarget(logger);
+        accessor.setApplication(getApplication());
+
+        appenders.addAll(accessor.getAppenders());
+      }
+    } catch (Exception e) {
+      logger.error("{}.getLoggerList() failed", getTarget(), e);
+    }
+    return appenders;
+  }
+
+  private static List<?> findServiceProviders(final ClassLoader cl)
+      throws NoSuchMethodException, SecurityException, ClassNotFoundException,
+      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    final Class<?> loggerFactory = cl.loadClass("org.apache.juli.logging.org.slf4j.LoggerFactory");
+    final Method findServiceProviders = loggerFactory.getDeclaredMethod("findServiceProviders");
+    // Make package protected accessible
+    findServiceProviders.setAccessible(true);
+    final List<?> providers = (List<?>) findServiceProviders.invoke(null);
+    findServiceProviders.setAccessible(false);
+    return providers;
+  }
+
+}
---
+++ psi-probe-core/src/main/java/psiprobe/tools/logging/slf4jlogback13/TomcatSlf4jLogback13LoggerAccessor.java
@@ -0,0 +1,206 @@
+/*
+ * Licensed under the GPL License. You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ */
+package psiprobe.tools.logging.slf4jlogback13;
+
+import com.google.common.collect.Iterators;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang3.reflect.MethodUtils;
+
+import psiprobe.tools.logging.DefaultAccessor;
+
+/**
+ * A wrapper for a TomcatSlf4jLogback logger.
+ */
+public class TomcatSlf4jLogback13LoggerAccessor extends DefaultAccessor {
+
+  /**
+   * Returns all appenders of this logger.
+   *
+   * @return a list of {@link TomcatSlf4jLogbackAppenderAccessor}s
+   */
+  @SuppressWarnings("unchecked")
+  public List<TomcatSlf4jLogback13AppenderAccessor> getAppenders() {
+    List<TomcatSlf4jLogback13AppenderAccessor> appenders = new ArrayList<>();
+
+    try {
+      for (Object appender : Collections.list(Iterators.asEnumeration(
+          (Iterator<Object>) MethodUtils.invokeMethod(getTarget(), "iteratorForAppenders")))) {
+        List<Object> siftedAppenders = getSiftedAppenders(appender);
+        if (siftedAppenders != null) {
+          for (Object siftedAppender : siftedAppenders) {
+            wrapAndAddAppender(siftedAppender, appenders);
+          }
+        } else {
+          wrapAndAddAppender(appender, appenders);
+        }
+      }
+    } catch (NoClassDefFoundError e) {
+      logger.error("{}#getAppenders() failed", getTarget().getClass().getName(), e);
+      logger.error("To see this logger, upgrade slf4j to 1.7.21+");
+    } catch (Exception e) {
+      logger.error("{}#getAppenders() failed", getTarget().getClass().getName(), e);
+    }
+    return appenders;
+  }
+
+  /**
+   * Returns the appender of this logger with the given name.
+   *
+   * @param name the name of the appender to return
+   * @return the appender with the given name, or null if no such appender exists for this logger
+   */
+  public TomcatSlf4jLogback13AppenderAccessor getAppender(String name) {
+    try {
+      Object appender = MethodUtils.invokeMethod(getTarget(), "getAppender", name);
+      if (appender == null) {
+        List<TomcatSlf4jLogback13AppenderAccessor> appenders = getAppenders();
+        for (TomcatSlf4jLogback13AppenderAccessor wrappedAppender : appenders) {
+          if (wrappedAppender.getIndex().equals(name)) {
+            return wrappedAppender;
+          }
+        }
+      }
+      return wrapAppender(appender);
+    } catch (Exception e) {
+      logger.error("{}#getAppender() failed", getTarget().getClass().getName(), e);
+    }
+    return null;
+  }
+
+  /**
+   * Checks if is context.
+   *
+   * @return true, if is context
+   */
+  public boolean isContext() {
+    return false;
+  }
+
+  /**
+   * Checks if is root.
+   *
+   * @return true, if is root
+   */
+  public boolean isRoot() {
+    return "ROOT".equals(getName());
+  }
+
+  /**
+   * Gets the name.
+   *
+   * @return the name
+   */
+  public String getName() {
+    return (String) getProperty(getTarget(), "name", null);
+  }
+
+  /**
+   * Gets the log level of this logger.
+   *
+   * @return the level of this logger
+   */
+  public String getLevel() {
+    try {
+      Object level = MethodUtils.invokeMethod(getTarget(), "getLevel");
+      return (String) MethodUtils.invokeMethod(level, "toString");
+    } catch (Exception e) {
+      logger.error("{}#getLevel() failed", getTarget().getClass().getName(), e);
+    }
+    return null;
+  }
+
+  /**
+   * Sets the log level of this logger.
+   *
+   * @param newLevelStr the name of the new level
+   */
+  public void setLevel(String newLevelStr) {
+    try {
+      Object level = MethodUtils.invokeMethod(getTarget(), "getLevel");
+      Object newLevel = MethodUtils.invokeMethod(level, "toLevel", newLevelStr);
+      MethodUtils.invokeMethod(getTarget(), "setLevel", newLevel);
+    } catch (Exception e) {
+      logger.error("{}#setLevel('{}') failed", getTarget().getClass().getName(), newLevelStr, e);
+    }
+  }
+
+  /**
+   * Gets the sifted appenders.
+   *
+   * @param appender the appender
+   * @return the sifted appenders
+   * @throws Exception the exception
+   */
+  @SuppressWarnings("unchecked")
+  private List<Object> getSiftedAppenders(Object appender) throws Exception {
+    if ("org.apache.juli.logging.ch.qos.logback.classic.sift.SiftingAppender"
+        .equals(appender.getClass().getName())) {
+
+      Object tracker = MethodUtils.invokeMethod(appender, "getAppenderTracker");
+      if (tracker != null) {
+        try {
+          return (List<Object>) MethodUtils.invokeMethod(tracker, "allComponents");
+        } catch (final NoSuchMethodException e) {
+          // XXX Legacy 1.0.x and lower support for logback
+          logger.trace("", e);
+          return (List<Object>) MethodUtils.invokeMethod(tracker, "valueList");
+        }
+      }
+      return new ArrayList<>();
+    }
+    return null;
+  }
+
+  /**
+   * Wrap and add appender.
+   *
+   * @param appender the appender
+   * @param appenders the appenders
+   */
+  private void wrapAndAddAppender(Object appender,
+      List<TomcatSlf4jLogback13AppenderAccessor> appenders) {
+
+    TomcatSlf4jLogback13AppenderAccessor appenderAccessor = wrapAppender(appender);
+    if (appenderAccessor != null) {
+      appenders.add(appenderAccessor);
+    }
+  }
+
+  /**
+   * Wrap appender.
+   *
+   * @param appender the appender
+   * @return the tomcat slf4j logback appender accessor
+   */
+  private TomcatSlf4jLogback13AppenderAccessor wrapAppender(Object appender) {
+    try {
+      if (appender == null) {
+        throw new IllegalArgumentException("appender is null");
+      }
+      TomcatSlf4jLogback13AppenderAccessor appenderAccessor =
+          new TomcatSlf4jLogback13AppenderAccessor();
+
+      appenderAccessor.setTarget(appender);
+      appenderAccessor.setLoggerAccessor(this);
+      appenderAccessor.setApplication(getApplication());
+      return appenderAccessor;
+    } catch (IllegalArgumentException e) {
+      logger.error("Could not wrap appender: '{}'", appender, e);
+    }
+    return null;
+  }
+
+}
