fc190a1a9d73a5d2f21eb55b13961bb16125cd1b
Jeremy Landis <jeremylandis@hotmail.com>
2023-05-13 23:56:20 -0400

    [ci] Use finals

--- psi-probe-core/src/main/java/psiprobe/Utils.java
+++ psi-probe-core/src/main/java/psiprobe/Utils.java
@@ -513,7 +513,7 @@ public final class Utils {
    *
    * @return the string
    */
-  protected static String leftPad(String str, int len, String fill) {
+  protected static String leftPad(final String str, final int len, final String fill) {
     if (str != null && str.length() < len) {
       return Strings.padStart(str, len, fill.charAt(0));
     }
