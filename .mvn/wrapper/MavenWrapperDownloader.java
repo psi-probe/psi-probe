1e0dd6cc02802280cc396245bbe03af459f13d11
Jeremy Landis <jeremylandis@hotmail.com>
2024-04-27 16:11:09 -0400

    [mvn] Update maven wrapper

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -21,42 +21,37 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.concurrent.ThreadLocalRandom;
 
-public final class MavenWrapperDownloader
-{
-    private static final String WRAPPER_VERSION = "3.2.0";
+public final class MavenWrapperDownloader {
+    private static final String WRAPPER_VERSION = "3.3.1";
 
     private static final boolean VERBOSE = Boolean.parseBoolean(System.getenv("MVNW_VERBOSE"));
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         log("Apache Maven Wrapper Downloader " + WRAPPER_VERSION);
 
-        if ( args.length != 2 )
-        {
+        if (args.length != 2) {
             System.err.println(" - ERROR wrapperUrl or wrapperJarPath parameter missing");
             System.exit(1);
         }
 
-        try
-        {
+        try {
             log(" - Downloader started");
-            final URL wrapperUrl = new URL( args[0] );
+            final URL wrapperUrl = URI.create(args[0]).toURL();
             final String jarPath = args[1].replace("..", ""); // Sanitize path
             final Path wrapperJarPath = Paths.get(jarPath).toAbsolutePath().normalize();
             downloadFileFromURL(wrapperUrl, wrapperJarPath);
             log("Done");
-        }
-        catch ( IOException e )
-        {
+        } catch (IOException e) {
             System.err.println("- Error downloading: " + e.getMessage());
-            if ( VERBOSE )
-            {
+            if (VERBOSE) {
                 e.printStackTrace();
             }
             System.exit(1);
@@ -64,33 +59,33 @@ public final class MavenWrapperDownloader
     }
 
     private static void downloadFileFromURL(URL wrapperUrl, Path wrapperJarPath)
-        throws IOException
-    {
+            throws IOException {
         log(" - Downloading to: " + wrapperJarPath);
-        if ( System.getenv("MVNW_USERNAME") != null && System.getenv("MVNW_PASSWORD") != null )
-        {
+        if (System.getenv("MVNW_USERNAME") != null && System.getenv("MVNW_PASSWORD") != null) {
             final String username = System.getenv("MVNW_USERNAME");
             final char[] password = System.getenv("MVNW_PASSWORD").toCharArray();
-            Authenticator.setDefault(new Authenticator()
-            {
+            Authenticator.setDefault(new Authenticator() {
                 @Override
-                protected PasswordAuthentication getPasswordAuthentication()
-                {
-                    return new PasswordAuthentication( username, password );
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
                 }
             });
         }
-        try ( InputStream inStream = wrapperUrl.openStream() )
-        {
-            Files.copy(inStream, wrapperJarPath, StandardCopyOption.REPLACE_EXISTING);
+        Path temp = wrapperJarPath
+                .getParent()
+                .resolve(wrapperJarPath.getFileName() + "."
+                        + Long.toUnsignedString(ThreadLocalRandom.current().nextLong()) + ".tmp");
+        try (InputStream inStream = wrapperUrl.openStream()) {
+            Files.copy(inStream, temp, StandardCopyOption.REPLACE_EXISTING);
+            Files.move(temp, wrapperJarPath, StandardCopyOption.REPLACE_EXISTING);
+        } finally {
+            Files.deleteIfExists(temp);
         }
         log(" - Downloader complete");
     }
 
-    private static void log(String msg)
-    {
-        if ( VERBOSE )
-        {
+    private static void log(String msg) {
+        if (VERBOSE) {
             System.out.println(msg);
         }
     }
