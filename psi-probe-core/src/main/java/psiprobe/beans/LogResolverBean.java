cc301defc07f4914aaecb584fbfdd35ac9f5f685
Jeremy Landis <jeremylandis@hotmail.com>
2025-05-23 23:13:23 -0400

    Move setup closer to usage

--- psi-probe-core/src/main/java/psiprobe/beans/LogResolverBean.java
+++ psi-probe-core/src/main/java/psiprobe/beans/LogResolverBean.java
@@ -233,16 +233,6 @@ public class LogResolverBean {
   public LogDestination getLogDestination(String logType, String webapp, boolean context,
       boolean root, String logName, String logIndex) {
 
-    LogDestination result = null;
-    Context ctx = null;
-    Application application = null;
-    if (webapp != null) {
-      ctx = getContainerWrapper().getTomcatContainer().findContext(webapp);
-      if (ctx != null) {
-        application = ApplicationUtils.getApplication(ctx, getContainerWrapper());
-      }
-    }
-
     // Supported loggers
     List<String> loggers = new ArrayList<>();
     loggers.add("jdk");
@@ -253,6 +243,16 @@ public class LogResolverBean {
     loggers.add("tomcatSlf4jLogback");
     loggers.add("tomcatSlf4jLogback13");
 
+    Context ctx = null;
+    Application application = null;
+    if (webapp != null) {
+      ctx = getContainerWrapper().getTomcatContainer().findContext(webapp);
+      if (ctx != null) {
+        application = ApplicationUtils.getApplication(ctx, getContainerWrapper());
+      }
+    }
+
+    LogDestination result = null;
     if (logName != null && "stdout".equals(logType)) {
       result = getStdoutLogDestination(logName);
     } else if (ctx != null && "catalina".equals(logType)) {
