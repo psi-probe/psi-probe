115d7338219ed7537700d93c8eaf91281a2e6d2d
Jeremy Landis <jeremylandis@hotmail.com>
2026-09-19 17:40:09 -0400

    Update deprecated code in Country usage

--- psi-probe-core/src/main/java/psiprobe/beans/ContainerListenerBean.java
+++ psi-probe-core/src/main/java/psiprobe/beans/ContainerListenerBean.java
@@ -373,8 +373,8 @@ public class ContainerListenerBean implements NotificationListener {
                   .withCache(new CHMCache()).build()) {
                 CountryResponse response =
                     reader.country(InetAddress.getByName(rp.getRemoteAddr()));
-                Country country = response.getCountry();
-                rp.setRemoteAddrLocale(new Locale("", country.getIsoCode()));
+                Country country = response.country();
+                rp.setRemoteAddrLocale(new Locale("", country.isoCode()));
               } catch (AddressNotFoundException e) {
                 logger.debug("Address Not Found: {}", e.getMessage());
                 logger.trace("", e);
